d2f2bbbb867c6e83f2a76a32333d4799d1fa21da
Peter <schmidt.multimedia@gmail.com>
2024-07-31 12:18:05 +0200

    02b / update

--- 02_SQL_Tables/02b_db_table_SQL.sql
+++ 02_SQL_Tables/02b_db_table_SQL.sql
@@ -15,20 +15,28 @@ CREATE DATABASE IF NOT EXISTS boo;
 USE boo;
 
 /* Tabelle test löschen, falls vorhanden*/
-
+DROP TABLE IF EXISTS test;
 
 /* Tab. test anlegen, falls noch nicht vorhanden*/
-
+CREATE TABLE IF NOT EXISTS test
+(
+	name VARCHAR(20) NOT NULL DEFAULT "TBA",
+	age INT NOT NULL DEFAULT 0
+);
 
 /* Alle Tabellen in der DB anzeigen */
-#SHOW TABLES;
+SHOW TABLES;
 
 /* Struktur der Tabelle anzeigen */
+DESCRIBE test;
 
 /* ----- Daten ------- */
-
+INSERT INTO test(name,age) VALUES ("Grizabella",29);
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
+INSERT INTO test VALUES ();
 
 -- ABER: Doppelte Datensätze werden zugelassen !
-
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
 
 /* ---- Inhalte der Tabelle anzeigen ---- */
+SELECT * FROM test;
